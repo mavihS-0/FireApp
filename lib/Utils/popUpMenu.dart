975a91088005e983f8703ff54877c0e27469b8b6
shivam-ajanitech <shivam.singh@ajanitech.com>
2023-09-02 14:16:57 +0530

    fixed bugs

--- lib/Utils/popUpMenu.dart
+++ lib/Utils/popUpMenu.dart
@@ -1,12 +1,15 @@
+import 'package:fire_app/Screens/splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 Widget customPopUpMenu() {
   return PopupMenuButton(
 //TODO: Popup menu
-      onSelected: (value){
-        if(value == 'Log Out'){
-          FirebaseAuth.instance.signOut();
+      onSelected: (value)async{
+        if(value == 'Log Out') {
+          await FirebaseAuth.instance.signOut();
+          Get.offAll(()=>SplashScreen());
         }
       },
       itemBuilder: (context){
