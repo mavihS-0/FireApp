8cfa98fc0f183b7117a904e42f581a0f38581e4c
shivam-ajanitech <shivam.singh@ajanitech.com>
2023-09-25 17:06:57 +0530

    notification settins and settings home page ui

---
+++ lib/Screens/SettingsScreens/notificationSettings.dart
@@ -0,0 +1,71 @@
+import 'package:fire_app/Utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class NotificationSettings extends StatefulWidget {
+  const NotificationSettings({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationSettings> createState() => _NotificationSettingsState();
+}
+
+class _NotificationSettingsState extends State<NotificationSettings> {
+
+  bool newMessageSwitch = true;
+  bool vibrateSwitch = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('New message notifications'),
+            trailing: Switch(
+              onChanged: (switchState){
+                setState(() {
+                  newMessageSwitch = switchState;
+                });
+              },
+              value: newMessageSwitch,
+              activeColor: Constants.secColor,
+              activeTrackColor: Constants.priColor,
+              inactiveThumbColor: Constants.priColor,
+              inactiveTrackColor: Constants.secColor,
+            ),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Ringtone'),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Vibrate'),
+            trailing: Switch(
+              onChanged: (switchState){
+                setState(() {
+                  vibrateSwitch = switchState;
+                });
+              },
+              value: vibrateSwitch,
+              activeColor: Constants.secColor,
+              activeTrackColor: Constants.priColor,
+              inactiveThumbColor: Constants.priColor,
+              inactiveTrackColor: Constants.secColor,
+            ),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('Ignore Battery Optimization'),
+            subtitle: Text('In order to make sure that all messages are delivered correctly you have to disable Battery Optimizations',style: TextStyle(
+              fontSize: 12,
+              color: Colors.black.withOpacity(0.42)
+            ),),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/Screens/SettingsScreens/settingsScreen.dart
+++ lib/Screens/SettingsScreens/settingsScreen.dart
@@ -1,76 +1,75 @@
+import 'package:fire_app/Screens/SettingsScreens/notificationSettings.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+import 'package:get/get.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
 
   @override
-  State<SettingsScreen> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-
-  List tileData= [
-    {'title' : 'Notifications',
-    'leadingIcon' : Icon(Icons.notifications),
-    'onPress' : (){}},
-    {'title' : 'Security',
-      'leadingIcon' : Icon(Icons.security),
-      'onPress' : (){}},
-    {'title' : 'Chat',
-      'leadingIcon' : Icon(Icons.chat),
-      'onPress' : (){}},
-    {'title' : 'Privacy Policy',
-      'leadingIcon' : Icon(Icons.privacy_tip_sharp),
-      'onPress' : (){}},
-    {'title' : 'About',
-      'leadingIcon' : Icon(Icons.info),
-      'onPress' : (){}},
-    {'title' : 'Delete Account',
-      'leadingIcon' : Icon(Icons.notifications),
-      'onPress' : (){}},
-  ];
-
-  @override
   Widget build(BuildContext context) {
+    List tileData= [
+      {'title' : 'Notifications',
+        'leadingIcon' : Icon(Icons.notifications),
+        'onPress' : (){
+          Get.to(()=>NotificationSettings());
+      }
+      },
+      {'title' : 'Security',
+        'leadingIcon' : Icon(Icons.security),
+        'onPress' : (){}},
+      {'title' : 'Chat',
+        'leadingIcon' : Icon(Icons.chat),
+        'onPress' : (){}},
+      {'title' : 'Privacy Policy',
+        'leadingIcon' : Icon(Icons.privacy_tip_sharp),
+        'onPress' : (){}},
+      {'title' : 'About',
+        'leadingIcon' : Icon(Icons.info),
+        'onPress' : (){}},
+      {'title' : 'Delete Account',
+        'leadingIcon' : Icon(Icons.notifications),
+        'onPress' : (){}},
+    ];
+
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            padding: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Settings'),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.symmetric(vertical: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white
+              ),
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemCount: tileData.length,
+                separatorBuilder: (context,index){
+                  return Divider(
+                    color: Colors.grey[200],
+                    thickness: 2,
+                  );
+                },
+                itemBuilder: (context,index){
+                  return ListTile(
+                    leading: tileData[index]['leadingIcon'],
+                    title: Text(tileData[index]['title'],style: TextStyle(fontWeight: FontWeight.bold),),
+                    trailing: Icon(Icons.arrow_forward_ios,size: 18,),
+                    onTap: tileData[index]['onPress'],
+                  );
+                },
+              ),
             ),
-            child: ListView.separated(
-              shrinkWrap: true,
-              itemCount: tileData.length,
-              separatorBuilder: (context,index){
-                return Divider(
-                  color: Colors.grey[200],
-                  thickness: 2,
-                );
-              },
-              itemBuilder: (context,index){
-                return ListTile(
-                  leading: tileData[index]['leadingIcon'],
-                  title: Text(tileData[index]['title']),
-                  trailing: Icon(Icons.arrow_forward_ios,size: 18,),
-                  onTap: tileData[index]['onPress'],
-                );
-              },
-            ),
-          ),
-          SizedBox(height: 10,),
-          Text('version1.0')
-        ],
-      )
+            SizedBox(height: 10,),
+            Text('version1.0')
+          ],
+        )
     );
   }
 }
+
 
