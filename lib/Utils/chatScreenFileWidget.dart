961042b01d78eaeb136ffa64ab6fbb6bf4eaee95
shivam-ajanitech <shivam.singh@ajanitech.com>
2023-09-29 17:57:23 +0530

    download function - fixed bug

--- lib/Utils/chatScreenFileWidget.dart
+++ lib/Utils/chatScreenFileWidget.dart
@@ -4,7 +4,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class ChatScreenFileWidget extends StatelessWidget {
   final Map fileData;
@@ -13,17 +12,15 @@ class ChatScreenFileWidget extends StatelessWidget {
   Future<void> downloadFile() async {
     Directory? directory;
     try{
-      directory = Directory('${(await getApplicationDocumentsDirectory()).path}/Downloads');
-      if(!await directory.exists()) directory.create();
-      directory = Directory('${directory.path}/Files');
-      if(!await directory.exists()) directory.create();
+      //directory = await getExternalStorageDirectory();
+      directory = Directory('/storage/16F2-1618/Android/data/com.example.fire_app/files');
       String filePath = '${directory!.path}/${fileData['content']['fileName']}';
       final file = File(filePath);
       final httpsReference = FirebaseStorage.instance.refFromURL(fileData['content']['fileURL']);
       await httpsReference.writeToFile(file);
       Get.snackbar("Download Complete", 'File downloaded successfully');
     }catch(e){
-      Get.snackbar("Error",e.toString());
+      Get.snackbar("Error",e.toString(),duration: Duration(seconds: 5));
     }
     return ;
   }
